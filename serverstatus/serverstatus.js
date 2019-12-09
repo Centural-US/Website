531a6c0211a055487c398fe1c1a088d5199546ec
Techno Cat <48845764+Sparrkxx@users.noreply.github.com>
2019-12-09 11:36:58 -0800

    changed js

--- serverstatus/serverstatus.js
+++ serverstatus/serverstatus.js
@@ -6,13 +6,12 @@ var url = "https://api.minetools.eu/ping/centural.us/25565";
 
 $.getJSON(url, function(r) {
     //data is the JSON string
- if(r.error){
-    $('#rest').html('Server Offline');
+    if(r.error){
+      $('#rest').html('Server Offline');
    return false;
  } 
 var pl = '';
  if(r.players.sample.length > 0 ){ pl = '<br>OP: '+r.players.sample[0].name;  } 
   $('#rest').html(r.description.replace(/§(.+?)/gi, '')+'<br><b>Players Online:</b> '+r.players.online+pl);
  $('#favicon').attr('src', r.favicon);
-    
 });
