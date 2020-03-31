a86897d2296ad2d14c424b8714770fdc3d37f41d
twango <48845764+twitch-twango@users.noreply.github.com>
2020-03-31 11:34:17 -0700

    Update showoncllick.js

--- assets/js/showoncllick.js
+++ assets/js/showoncllick.js
@@ -1,8 +1,8 @@
 function showclick() {
     var x = document.getElementById("voteButton");
     if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
       x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
   }
